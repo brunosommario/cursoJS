b38bd868916114ecb18ad92a5893abdba17577cd
Bruno Sommario Turoni <sommariobruno@gmail.com>
2024-04-17 19:32:40 +0200

    creando archivos html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let nombre ='bruno'
+console.warn('esto es un warn')
+let apellidos = "Sommario Turoni"
+let PlatoPrincipa = "arroz con pollo";
+let PlatoSegundario ="empanadas de carne";
+const restaurante = "Las milangas de Bruni";
+let platoelegido = prompt ('que te gustaria cenar?')
+alert ('la noche se presta para cenar'+" "+ PlatoPrincipa);
+let platoprincipa = 35;
+let bebida = 5; 
+let postre = 4;
+let menucena = platoprincipa + bebida + postre;
